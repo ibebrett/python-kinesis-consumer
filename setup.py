6abefc22a892d16e60da5de26c01dd4e45526e42
Brett Jurman <i.be.brett@gmail.com>
2014-10-20 20:39:22 -0400

    changed description

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='Python Kinesis Consumer',
     version='0.1',
-    description='System for processing kinesis data for python',
+    description='System for processing kinesis streams for python',
     author='Brett Jurman',
     author_email='i.be.brett@gmail.com',
     packages=find_packages(),
